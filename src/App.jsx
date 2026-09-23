d3570afb132b3d774527df69a1973f9a45f6c4e6
Kouno <info.koyuma@gmail.com>
2026-09-23 13:30:53 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
                 {
                     params: {
                         query: term,
-                        per_page: 33,
+                        per_page: 30,
                     },
                     headers: {
                         Authorization: `Client-ID ${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`,
